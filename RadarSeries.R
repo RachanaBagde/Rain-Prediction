36afdf76ca0621cd15cd839876d265c34eb23b4c
Rachana I. Bagde <rachanabagde1996@gmail.com>
2015-04-03 01:19:43 +0530

    Update RadarSeries.R

--- RadarSeries.R
+++ RadarSeries.R
@@ -21,3 +21,19 @@
  
  test <- dt_test[,RadarSeries := cumsum(NewRadarIndicator),by=Id]
 
+#The line below finds all the times where the time to end of observations increases from the prior reading and adds
+#a column TimeToEndInversion where 0 equals no increase in time and 1 when the time increases.
+#This is one of the indicators that the next set of observations is from a different radar station. 
+
+#dt[,TimeToEndInversion:=c(1,(sign(diff(TimeToEnd))+1)/2),by=Id]
+
+#The line above works almost all the time, but sometimes there is no time to end inversion and the only indication we get 
+#is that the radar distance changes.  This next line finds all of those times and indicates it with a one.
+
+#dt[,NewDistanceToRadar:=c(1,abs(sign(diff(DistanceToRadar)))),by=Id]
+
+#Finally, both of the two indicators work almost all the time to indicate that the next sequence of observations are from 
+#a new radar.  But since neither work all the time, they are combined with an OR to catch (hopefully) all the time that a 
+#new radar sequence starts.
+
+#dt[,NewRadarIndicator:=TimeToEndInversion | NewDistanceToRadar]
